714028f4e36fdbfbe8a7ab62cdbd3cbe7403cba2
Tyler Wilbanks <ryan.wilbanks45@gmail.com>
2023-08-07 13:07:39 -0500

    tweak keyboard size, add icons

--- app/src/main/java/com/minutesock/wordgame/presentation/components/FalseKeyboardLetter.kt
+++ app/src/main/java/com/minutesock/wordgame/presentation/components/FalseKeyboardLetter.kt
@@ -2,13 +2,19 @@ package com.minutesock.wordgame.presentation.components
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.minutesock.wordgame.presentation.DailyWordEvent
@@ -21,11 +27,14 @@ fun FalseKeyboardLetter(
     onEvent: (DailyWordEvent) -> Unit,
     displayText: String
 ) {
+    val isLetter = displayText.length == 1
+    val sizeX = if (isLetter) 38.dp else 50.dp
     TextButton(
         modifier = Modifier
-            .size(38.dp, 64.dp)
+            .size(sizeX, 55.dp)
             .padding(2.dp),
         colors = ButtonDefaults.buttonColors(containerColor = Color.Gray),
+        shape = RoundedCornerShape(10),
         onClick = {
             when (displayText) {
                 "enter" -> onEvent(DailyWordEvent.OnEnterPress)
@@ -34,10 +43,15 @@ fun FalseKeyboardLetter(
             }
         }
     ) {
-        Text(
-            text = displayText,
-            color = Color.White,
-            fontSize = 16.sp
-        )
+        when (displayText) {
+            "enter" -> Icon(imageVector = Icons.Default.CheckCircle, contentDescription = "enter", tint = Color.White)
+            "remove" -> Icon(imageVector = Icons.Default.Clear, contentDescription = "remove", tint = Color.White)
+            else -> Text(
+                textAlign = TextAlign.Center,
+                text = displayText,
+                color = Color.White,
+                fontSize = 16.sp
+            )
+        }
     }
 }
