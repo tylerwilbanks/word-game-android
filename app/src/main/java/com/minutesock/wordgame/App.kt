cfa87331088fc4d66e8b2c85e642b23e1130bfc3
Tyler Wilbanks <ryan.wilbanks45@gmail.com>
2023-08-27 15:58:23 -0500

    remove unnecessary application, begin implementing bottom nav bar

--- app/src/main/java/com/minutesock/wordgame/App.kt
+++ app/src/main/java/com/minutesock/wordgame/App.kt
@@ -7,7 +7,6 @@ import com.minutesock.wordgame.data.AppDatabase
 
 class App : Application() {
 
-
     override fun onCreate() {
         super.onCreate()
         App.applicationContext = applicationContext
@@ -18,19 +17,6 @@ class App : Application() {
         val database by lazy { AppDatabase.getDatabase(applicationContext) }
 
         lateinit var applicationContext: Context
-            private set
-    }
-}
-
-class MyApplication : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        appContext = applicationContext
-    }
-
-    companion object {
-        var appContext: Context? = null
             private set
     }
 }
---
+++ app/src/main/java/com/minutesock/wordgame/domain/BottomNavItem.kt
@@ -0,0 +1,18 @@
+package com.minutesock.wordgame.domain
+
+import com.minutesock.wordgame.R
+
+sealed class BottomNavItem(
+    val titleId: Int,
+    val icon: Int,
+) {
+    data object Daily : BottomNavItem(
+        titleId = R.string.daily,
+        icon = R.drawable.baseline_today_24
+    )
+
+    data object Profile : BottomNavItem(
+        titleId = R.string.profile,
+        icon = R.drawable.baseline_person_24
+    )
+}
