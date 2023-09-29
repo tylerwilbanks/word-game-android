b91ca276ea48aa98652aff2eea444cbb120fdd36
tyler <tyler.wilbanks45@gmail.com>
2023-09-28 22:40:27 -0500

    add better dark mode check in App

--- core/src/main/java/com/minutesock/core/App.kt
+++ core/src/main/java/com/minutesock/core/App.kt
@@ -18,7 +18,11 @@ class App : Application() {
         val database by lazy { AppDatabase.getDatabase(applicationContext) }
 
         val isDarkMode by lazy {
-            applicationContext.resources.configuration.uiMode == Configuration.UI_MODE_NIGHT_YES
+            when (applicationContext.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                Configuration.UI_MODE_NIGHT_YES -> true
+                Configuration.UI_MODE_NIGHT_NO -> false
+                else -> false
+            }
         }
 
         lateinit var applicationContext: Context
